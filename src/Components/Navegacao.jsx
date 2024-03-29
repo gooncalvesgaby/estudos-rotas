ee29041e67e9076fcc4b4dd455fb895d799e09b0
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-03-28 23:50:10 -0300

    estilizando componentes

--- src/Components/Navegacao.jsx
+++ src/Components/Navegacao.jsx
@@ -3,27 +3,27 @@ import Inicio from "./Inicio.jsx"
 import Sobre from "./Sobre.jsx"
 import Projetos from "./Projetos.jsx"
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import * as S from "./Style.jsx"
 
 function Navegacao() {
     return (
       <BrowserRouter>
-  
-        <nav>
-          <ul>
+      <S.Navegacao>
+        <S.Lista>
             <li> <Link to="/"> Início </Link> </li>
             <li> <Link to="/Sobre"> Sobre </Link> </li>
             <li> <Link to="/Projetos"> Projetos </Link></li>
-          </ul>
-        </nav>
+        </S.Lista>
+        </S.Navegacao>
   
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="Sobre" element = { <Sobre /> } />
-          <Route path="Projetos" element = { <Projetos /> } />
-  
+          <Route path="Projetos" element = { <Projetos /> } />  
         </Routes>
   
       </BrowserRouter>
+      
     )
   }
 
